94957d8786c42f19aec6bc1ed2eae0eb461b01d4
buzz9248 <buzz9248@naver.com>
2025-04-02 17:36:43 +0900

    app.jsx basename 수정

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,36 @@
 // }
 
 // export default App
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./routes/Home";
+// import Detail from "./routes/Detail";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/movie/:id" element={<Detail />} />
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/recommended_movie_card_board">
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
+        {/* 루트 경로 */}
         <Route path="/" element={<Home />} />
+
+        {/* 영화 상세 정보 */}
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/recommended_movie_card_board', // 깃허브 리포지토리와 같게
+  base: '/recommended_movie_card_board/', // 깃허브 리포지토리와 같게
   plugins: [react()],
-  // server: {
-  //   fs: {
-  //     strict: false,
-  //   },
-  //   historyApiFallback: true,
-  // },
+  server: {
+    fs: {
+      strict: false,
+    },
+    historyApiFallback: true,
+  },
 })
